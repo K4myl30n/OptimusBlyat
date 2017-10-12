80ef285fe4e951f5b17aadb5a9464b6ab81b3dc2
K4myl30n <kamil007p@gmail.com>
2017-10-12 08:46:04 +0200

    Odczyt i wstawianie danych

--- sql_zadanie1/dane.py
+++ sql_zadanie1/dane.py
@@ -13,11 +13,29 @@ def dane_z_pliku(plik):
             dane.append(rekord)
     return dane
     
+    
+def wyczysc_dane(dane, pole):
+    for i, rekord in enumerate(dane):
+        element = rekord[pole]
+        element = element.replace(" ","")
+        element = element.replace(",",".")
+        element = element.replace("zł","")
+        rekord[pole] = element
+        dane[i] = rekord
+    return dane
+    
 
 
 def main(args):
     premia = dane_z_pliku('premia.txt')
+    premia = wyczysc_dane(premia, 1)
     print(premia)
+    
+    pracownicy = dane_z_pliku('pracownicy.txt')
+    pracownicy = wyczysc_dane(pracownicy, 5)
+    print(pracownicy)
+    
+    dzial = dane_z_pliku('dzial.txt')
     
     return 0
 
